e0c7d7e31dd38bbb609bb6b83cf290811ac29401
voicedhealer <vivien.bernardot@gmail.com>
2025-09-27 12:27:32 +0200

    fix V1.07 - syntax incorrect + chmod - exercice print_square.js

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 
-if (process.argv.length !== 3) {
-    console.log('Usage: ./1-print_square.js <size>');
-    process.exit(1);
-}
-
 const size = parseInt(process.argv[2], 10);
-if (isNaN(size) || size < 1) {
-    console.log('Please provide a positive integer');
-    process.exit(1);
+
+if (isNaN(size) || size < 0) {
+    process.exit(0);
 }
 
-// Construire une ligne du carré
 const line = '#'.repeat(size);
-// Afficher la ligne n fois
+
 for (let i = 0; i < size; i++) {
     console.log(line);
 }
